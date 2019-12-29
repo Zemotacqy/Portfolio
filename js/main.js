0a9f5aad92bf321a140f036ddc9b2bb4e14af954
Manish Ranjan <mranjan1398@gmail.com>
2019-12-30 02:15:10 +0530

    Updates

---
+++ js/main.js
@@ -0,0 +1,40 @@
+// Select DOM Items
+const menuBtn = document.querySelector('.menu-btn');
+const menu = document.querySelector('.menu');
+const menuNav = document.querySelector('.menu-nav');
+const menuBranding = document.querySelector('.menu-branding');
+const navItems = document.querySelectorAll('.nav-item');
+const Body = document.getElementsByTagName("body")[0];
+
+// Set the initial state of Menu
+let showMenu = false;
+
+menuBtn.addEventListener('click', toggleMenu);
+
+function toggleMenu() {
+    if(!showMenu) {
+        menuBtn.classList.add('close');
+        menu.classList.add('show');
+        menuNav.classList.add('show');
+        menuBranding.classList.add('show');
+        navItems.forEach( item => {
+            item.classList.add('show');
+        });
+        Body.style.overflow = "hidden";
+
+        // Set Menu State
+        showMenu = true;
+    } else {
+        menuBtn.classList.remove('close');
+        menu.classList.remove('show');
+        menuNav.classList.remove('show');
+        menuBranding.classList.remove('show');
+        navItems.forEach( item => {
+            item.classList.remove('show');
+        });
+        Body.style.overflow = "scroll";
+        
+        // Set Menu State
+        showMenu = false;
+    }
+}
