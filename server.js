f984bda3f1d6a0bc7e204e57210baf44eed3f8b4
Manish Ranjan <mranjan1398@gmail.com>
2019-12-31 19:49:12 +0530

    created server

---
+++ server.js
@@ -0,0 +1,29 @@
+var static = require('node-static');
+var http = require('http');
+var fs = require('fs');
+
+var PORT = 8080;
+var logFilePath = './log';
+var content = new static.Server('./dist', { cache: 3600 });
+
+function WriteToLog(path, address) {
+    var timestamp = new Date().toUTCString();
+    var data = timestamp + "\t" + path + "\t" + address + "\n";
+    fs.appendFileSync(logFilePath, data);
+}
+
+http.createServer((req, res) => {
+    
+    req.addListener('end', () => {
+        content.serve(req, res, (e, res) => {
+            if(e && (e.status==404)) {
+                content.serveFile('./dist/404.html', 404, {}, req, res);
+            }
+            WriteToLog(req.url, req.connection.remoteAddress);
+        });
+    }).resume();
+
+}).listen(PORT, ()=>{
+    console.log(`Server running on port ${PORT}.`);
+    WriteToLog("", `Server running on port ${PORT}.`);
+});
